3c96a23200d72bf31945ff12f5fa8a0076e5144e
geekbrains <ashulyaev93@mail.ru>
2021-01-02 15:12:46 +0400

    Магазин запустился, сущность categories создана. Осталось сделать саму страницу с категориями и пагинацией.

--- src/main/java/com/geekbrains/spring/lesson6/repositories/ProductRepository.java
+++ src/main/java/com/geekbrains/spring/lesson6/repositories/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.geekbrains.spring.lesson6.repositories;
 
 import com.geekbrains.spring.lesson6.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
+    @Query("SELECT p FROM Product p JOIN p.category c WHERE c.code = :code")
+    Page<Product> findProductByCategoryCode(@Param("code") String code, Pageable pageable);
 }
